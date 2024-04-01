aa1a7537a08516a6375b9df8d2c23d35c626cebb
azalea <azaleawang4@gmail.com>
2024-04-01 23:03:57 +0800

    docs: add about info

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,10 +1,14 @@
 <template>
-  <q-page class="row q-pa-lg">
-    <h5 class="q-mt-none">About</h5>
-
+  <q-page class="col q-pa-lg">
+    <h4 class="q-mt-none">About</h4>
+    <section>
+      <ul class="text-body1">
+        <li>Language: TypeScript</li>
+        <li>UI Framework: Quasar + Vue.js 3</li>
+        <li>Build tool: Vite</li>
+      </ul>
+    </section>
   </q-page>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
